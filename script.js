dff76ba82615f6b4da89649a74d0a94a76d52ca7
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-14 17:30:02 +0000

    2 player but errors present

--- script.js
+++ script.js
@@ -12,43 +12,92 @@ const dieThree = new DieFace(3, `./img/dice3.png`);
 const dieFour = new DieFace(4, `./img/dice4.png`);
 const dieFive = new DieFace(5, `./img/dice5.png`);
 const dieSix = new DieFace(6, `./img/dice6.png`);
+let diePlaceholder = `<img src="./img/dicePlaceholder.png" alt=dieplaceholder>`;
 
 //This is the six-sided die. This would need to be changed to a class if we wanted to open it up to dice with different numbers of faces
 let die = [dieOne, dieTwo, dieThree, dieFour, dieFive, dieSix];
 
-//Player class. Needs to have variables for name, current total and number of rolls taken so far.
-//TODO
-
 //Record screen elements for manipulation
-const roll = document.getElementById(`roll`);
 const titleMessage = document.getElementById(`start-message`);
 const begin = document.getElementById(`begin`);
+const roll = document.getElementById(`roll`);
+const hold = document.getElementById(`hold`);
 const restart = document.getElementById(`restart`);
 const result = document.getElementsByClassName(`result`)[0];
-const counter = document.getElementsByClassName(`counter`)[0];
-const player1 = document.getElementById(`player1`);
-const player2 = document.getElementById(`player2`);
+const counterp1 = document.getElementsByClassName(`player1counter`)[0];
+const counterp2 = document.getElementsByClassName(`player2counter`)[0];
+const heldp1 = document.getElementsByClassName(`player1held`)[0];
+const heldp2 = document.getElementsByClassName(`player2held`)[0];
+const screenPlayer1 = document.getElementById(`player1`);
+const screenPlayer2 = document.getElementById(`player2`);
 
-let runningTotal = 0;
-let numberOfRolls = 0;
-let diePlaceholder = `<img src="./img/dicePlaceholder.png" alt=dieplaceholder>`;
-let player1name = `Player1`;
+//Player class. Needs to have variables for name, current total and number of rolls taken so far.
+
+class Player {
+    constructor(name, screenCurrent, screenHeld, screenName){
+        this.name = name;
+        this.current = 0;
+        this.held = 0;
+        this.numberOfRolls;
+        this.screenCurrent = screenCurrent;
+        this.screenHeld = screenHeld;
+        this.screenName = screenName;
+    }
+
+    resetPlayer() {
+    this.current = 0;
+    this.held = 0;
+    this.screenCurrent.textContent = `counter: ${this.current}`;
+    this.screenHeld.textContent = `held: ${this.held}`;
+    this.screenCurrent.style.visibility = "visible";
+    this.screenHeld.style.visibility = "visible";
+    this.numberOfRolls = 0;
+    this.screenName.style.visibility = "visible";
+    this.screenName.textContent = this.name;
+    }
+
+    updateWithDieResult(resultDie) {
+        result.innerHTML = `<img src="${resultDie.image}" alt=${resultDie.number}>`;
+    
+        this.current += resultDie.number;
+        this.numberOfRolls++;
+        this.screenCurrent.textContent = `counter: ${this.current}`;
+    }
+
+    checkGameOver (resultDie) {
+        if (this.held > 20) {
+            titleMessage.style.visibility = "visible";
+            titleMessage.textContent = `${this.name} WINS!`;
+            roll.style.visibility = "hidden";
+            restart.style.visibility = "visible";
+        } else if (resultDie.number == 1) {
+            this.current = 0;
+            this.screenCurrent.textContent = this.current;
+            changeActivePlayer();
+        }
+    }
+}
+
+let player1 = new Player('PlayerA',counterp1,heldp1,screenPlayer1);
+let player2 = new Player('PlayerB',counterp2,heldp2,screenPlayer2);
+//variables to allow for player switching.
+let activePlayer = player1;
 
 //Code for rolling dice etc
 
 const initialiseGame = () => {
     roll.style.visibility = "visible";
+    hold.style.visibility = "visible";
     titleMessage.style.visibility = "hidden";
     begin.style.visibility = "hidden";
-    player1.style.visibility = "visible";
     restart.style.visibility = "hidden";
     result.style.visibility = "visible";
     result.innerHTML = diePlaceholder;
-    runningTotal = 0;
-    counter.textContent = runningTotal;
-    counter.style.visibility = "visible";
-    numberOfRolls = 0;
-    player1.textContent = player1name;
+    player1.resetPlayer();
+    player2.resetPlayer();
+    if (activePlayer == player2){       //to make sure player1 always starts. Could be neater since the logic is within changeActivePlayer() also
+        changeActivePlayer();
+    }
 }
 
 const rollDie = () => {
@@ -56,31 +105,37 @@ const rollDie = () => {
     return die[randomNumber - 1];
 }
 
-const updateWithDieResult = (resultDie) => {
-    result.innerHTML = `<img src="${resultDie.image}" alt=${resultDie.number}>`;
-
-    runningTotal += resultDie.number;
-    numberOfRolls++;
-    counter.textContent = runningTotal;
-}
-
-const checkGameOver = (resultDie) => {
-    if (runningTotal >= 20) {
-        player1.textContent = `YOU WIN YOU WIN`;
-        roll.style.visibility = "hidden";
-        restart.style.visibility = "visible";
-    } else if (resultDie.number == 1) {
-        player1.textContent = `YOU LOSE YOU LOSE`;
-        roll.style.visibility = "hidden";
-        restart.style.visibility = "visible";
+//changes the active player to be whatever the other player is
+//quite brittle, would need changing to support more players
+//could probably be done with an array of players instead
+const changeActivePlayer = () => {
+    activePlayer.current = 0;
+    activePlayer.screenCurrent.textContent = `current: ${activePlayer.current}`;
+    activePlayer.screenCurrent = activePlayer.current;
+    if(activePlayer == player1){
+        activePlayer = player2;
+        player1.screenName.style.color=`black`;
+        player2.screenName.style.color=`red`;
+    } else{
+         activePlayer = player1;
+         player1.screenName.style.color=`red`;
+        player2.screenName.style.color=`black`;
     }
 }
 
 roll.addEventListener(`click`, () => {
     let resultDie = rollDie();
-    updateWithDieResult(resultDie);
-    checkGameOver(resultDie);
+    activePlayer.updateWithDieResult(resultDie);
+    activePlayer.checkGameOver(resultDie);
 });
+
+hold.addEventListener(`click`, () => {
+    activePlayer.held += activePlayer.current;
+    console.log(activePlayer.held);
+    activePlayer.screenHeld.textContent = `held: ${activePlayer.held}`;
+    activePlayer.checkGameOver(2);  //passing this a 2 just so that it isn't a 1, since we only want to check if the player has won
+    changeActivePlayer();
+})
 
 restart.addEventListener(`click`, () => {
     initialiseGame();
